6c37bde8a7d4e736a5cd144956345e6fe7236c1e
Chandrakant Gorain <143680837+CKofficial-1797@users.noreply.github.com>
2025-03-23 12:09:43 +0530

    Update AdminUsers.jsx

--- src/admin/Users/AdminUsers.jsx
+++ src/admin/Users/AdminUsers.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./users.css";
+import "./Users.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { server } from "../../main";

ded2652a2e699d7c1abadc3d3759d5d7f6945c4d
Chandrakant Gorain <143680837+CKofficial-1797@users.noreply.github.com>
2025-03-23 12:00:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CourseContextProvider } from './context/CourseContext.jsx';
 import App from './App.jsx';
 import { UserContextProvider } from './context/userContext.jsx';
 
-export const server = 'http://localhost:3000';
+export const server = 'https://vidyarthi-tewf.onrender.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
